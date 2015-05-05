2dce7c1093f1143c8a4e805a8b03ac8f3a8d7e36
christianfeo <christianfeob@yahoo.com>
2015-05-05 15:44:34 -0430

    Added feedback on button depending on option

--- popup.js
+++ popup.js
@@ -6,4 +6,17 @@ $(document).ready(function(){
 	});
 
 
+	$("input[name='typeSearch']").on("change", function(){
+
+		var curr = $(this).val().trim(); // automatic or direct
+		
+		if(curr === "direct")
+		{
+			$("button#go").text("Go Now!");
+		}
+		else
+			$("button#go").text("Update Settings");
+
+	});
+
 });
