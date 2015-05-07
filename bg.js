5b33a2175b46e1cb7cd63b0b1d1cbebf80ab54c3
christianfeo <christianfeob@yahoo.com>
2015-05-07 16:27:02 -0430

    Cleaning code

--- bg.js
+++ bg.js
@@ -1,5 +1,3 @@
-var tabs = [];
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	
 		if(request.resetLog !== undefined) 
@@ -9,7 +7,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 
 	if(request.status.trim() === "automatic-scan")
 	{
-		log("requested automatic scan...");
+		log("Requested automatic scan...");
 		start();
 	}
 
@@ -23,22 +21,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 		}
 });
 
-function containsTab(id)
-{
-	var flag = false;
-
-
-	for(var i = 0; i<tabs.length; i++)
-	{
-		if(id === tabs[i])
-		{
-			flag = true;
-		}
-	}
-
-	return flag;
-}
-
 function log(str) {
 
 	var d = new Date();
@@ -49,65 +31,45 @@ function log(str) {
 	str = str.replace(/&/g, "&amp;");
 
 	localStorage["log"] += "[" + time + "] " + str + "\n";
-
 }
 
-var totalWords = 0;
-var checkoutTab = -1;
-var since = 0; //get tweets posted after this ID.
-var immuneTabs = [];
-var oldUrls = [];
 var keywords = [];
 var timer = null;
-var excludeStr = "";
-var readyTabs = 0;
 var interval = 1000;
-var getTweetsTime = "";
-var getTweetsTotal = 0;
-var logSince = 0;
 
 function start() {
-	log("Start() activated...");
 
 	var kwStr = localStorage["keywords"];
-
 	var date = (new Date).toLocaleDateString();
-	log("Starting bot - " + date);
+	var d = new Date();
+	var time = d.toLocaleTimeString();
 
-	if(kwStr !== undefined && kwStr.length > 0) {
-		log("inside if...");
-		
-		oldUrls = [];
+	log("Bot starting at " + date + "  " + time);
 
-		log("after orlUrls");
+	if(kwStr !== undefined && kwStr.length > 0) 
+	{
 
 		keywords = kwStr.split(",");
 
-		log("afte rkeywords get...["+localStorage["keywords"]+"]");
-
-		readyTabs = 0;
-		totalWords = keywords.length;
-		checkoutTab = -1;
-		since = 0;
-		tabs = [];
-		immuneTabs = [];
-		//timer = setInterval(run, interval);
+		log("Using keywords ["+localStorage["keywords"]+"]");
 		
 		run();
 
-	} else
+	} 
+	else
 		log("Stopping. No keywords defined.");
 }
 
 function run() {
-	log("on the RUN() method");
+	log("Running...");
 
-	try {
-		log("checking...twitter...");
+	try 
+	{
 		var account = 'nikestore';
+		log("Checking twitter [@"+account+"]...");
 
 		$.ajax({
-				url: 'https://twitter.com/i/profiles/show/' + account + '/timeline?since_id=' + since,
+				url: 'https://twitter.com/i/profiles/show/' + account + '/timeline?since_id=' + 0,
 				type: "GET",
 				success: getLatestTweet,
 				error: function(xhr, textStatus, err) {
@@ -121,8 +83,9 @@ function run() {
 				}
 		});
 
-		log("finished ajax...");
-	} catch(err) {
+	} 
+	catch(err) 
+	{
 		log("run() ERROR: " + err.message);
 		//run();
 	}
@@ -131,7 +94,7 @@ function run() {
 
 function getLatestTweet(data)
 {
-	log("retrieving latest tweet JSON...");
+	log("Retrieving the latest tweet...");
 
 	var html = data.items_html;
 	html = html.replace(/’/g, "'");
@@ -141,51 +104,22 @@ function getLatestTweet(data)
 	var stIndex = html.indexOf('dir="ltr" data-aria-label-part="0"');
 	var enIndex = html.indexOf('class="js-tweet-details-fixer tweet-details-fixer"',stIndex);
 
-	log("start index = " + stIndex + ", end index = " + enIndex);
+	//log("start index = " + stIndex + ", end index = " + enIndex);
 
 	var tweet = html.substring(stIndex, enIndex);
 	
 	//log("latest tweet = " + tweet);
 
 	var url = retrieveUrlFromTweet(tweet);
+
+	log("URL = " + url);
 }
 
 function retrieveUrlFromTweet(tweet)
 {
 	// url is inside data-expanded-url="url here" followed by class="twitter-timeline-link"
-	var stIndex = tweet.indexOf('data-expanded-url="');
+	var stIndex = tweet.indexOf('data-expanded-url="') + String('data-expanded-url="').length;
 	var enIndex = tweet.indexOf('class="twitter-timeline-link"') - 2;
 
-	var url = tweet.substring(stIndex,enIndex);
-
-	log("The tweets url is = " + url);
-}
-
-// Get the latest tweet
-function getTweets(data) {
-
-	log("Retrieving tweets...");
-
-	try {
-
-			if(data.items_html !== '')
-			{
-				log("data items_html is not undefined...");
-				getTweetsTime = (new Date()).toLocaleTimeString();
-				getTweetsTotal++;
-
-				var html = data.items_html;
-				html = html.replace(/’/g, "'");
-
-				log("html 'cleaned' = " + html);
-
-				var urls = getUrls(html);
-			}
-				
-
-		} catch(err) {
-			log("getTweets() ERROR: " + err.message);
-			run();
-		}
-
+	return tweet.substring(stIndex,enIndex);
 }
